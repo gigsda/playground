ac1c8604ffa3a6a622b482639395e17e5ec06c45
tim-yang <tim.yang@nexr.com>
2013-01-28 14:16:35 +0900

    adding new project

---
+++ test1/src/test2.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: gigsda
+ * Date: 1/14/13
+ * Time: 5:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class test2 {
+}
